33eadd40493f8c4a438441e8f7a35f51347e3510
Adam Parker <adam.parker@economicmodeling.com>
2022-03-04 12:24:31 -0500

    updated code

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,5 +6,38 @@ export type AmplifyDependentResourcesAttributes = {
             "Region": "string",
             "LambdaExecutionRole": "string"
         }
+    },
+    "storage": {
+        "ebgweather": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "ebgweather": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        },
+        "ebgweatherrest": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "auth": {
+        "ebgweather57278976": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import './App.css';
-import amplify from 'amplify';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+
+import { listHistories } from "./graphql/queries";
+import { createHistory } from "./graphql/mutations";
 
 function App() {
-
   const [weatherJSON, setWeatherJSON] = useState({});
+  const [history, setHistory] = useState([]);
 
   useEffect(() => {
-    setWeatherJSON({test: true});
-  },
-  [])
+    const run = async () => {
+      const resp = await axios.get('https://7oiwikjha1.execute-api.us-east-1.amazonaws.com/dev/weather');
+      setWeatherJSON(resp.data);
+      const histories = await API.graphql(graphqlOperation(listHistories));
+      alert(histories);
+      setHistory({ test: true });
+    };
+
+    run();
+  }, []);
+
+  const saveData = async () => {
+    await API.graphql(graphqlOperation(createHistory, { input: { id: Math.round(Math.random() * 10000), name: "Atlanta", date: "2022-03-03", temperature: "75",  }}))
+  };
 
   return (
-    <div className="App">
+    <div className="App" style={{padding: "20px" }}>
       <h1>The current weather is: </h1>
+      <input type="submit" onClick={saveData} value="record data"/>
       <div>{JSON.stringify(weatherJSON)}</div>
+
+      <h1>History</h1>
+      <div>{JSON.stringify(history)}</div>
     </div>
   );
 }
